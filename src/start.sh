b6fb8b1861712e96731f9bef244180952f2f35ca
Tim Pietrusky <timpietrusky@gmail.com>
2023-12-11 22:03:50 +0100

    fix: removed libtcmalloc detection as this is not needed for this basic example

--- src/start.sh
+++ src/start.sh
@@ -1,8 +1,4 @@
 #!/bin/bash
 
-# Use libtcmalloc for better memory management
-TCMALLOC="$(ldconfig -p | grep -Po "libtcmalloc.so.\d" | head -n 1)"
-export LD_PRELOAD="${TCMALLOC}"
-
 echo "runpod-worker-helloworld: Starting RunPod Handler"
 python3 -u /rp_handler.py
